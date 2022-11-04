f07d080db27d672326f2800f11069248810c58c5
theokeanu <theodore.keanu@gmail.com>
2022-11-04 20:21:29 +0700

    Inventory
    Menambahkan item kunci dan placeholder binatang yang bisa diambildan muncul di UI

---
+++ AnimalTrafficJam/Assets/Script/KeyUse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUse : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use()
+    {
+        Instantiate(effect, player.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
